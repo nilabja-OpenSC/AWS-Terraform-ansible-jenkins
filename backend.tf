3d0202aa6adb5f54f5639e3d2a0cfdf4abaa843b
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-05-05 22:48:46 +0530

    Add files via upload

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
        region = "us-east-1"
        profile = "default"
        key = "terraformstatefile"
-       bucket = "terraformstatebucker6655"
+       bucket = "terraformstatebucker66555"
 
     }
 }
