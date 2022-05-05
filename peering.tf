902b847f0bea3814058e7a0ae264e852b05633df
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-05-05 23:38:45 +0530

    Add files via upload

--- peering.tf
+++ peering.tf
@@ -1,6 +1,67 @@
-#resource "aws_vpc_peering_connection" "useast1-uswest2" {
-#    provider = aws.region_master
-#    peer_vpc_id = aws_vpc.vpc_worker.id
-#    vpc_id = aws_vpc.vpc_master.id
-#    peer_region = var.region_worker
-#}
+resource "aws_vpc_peering_connection" "useast1-uswest2" {
+    provider = aws.region_master
+    peer_vpc_id = aws_vpc.vpc_worker.id
+    vpc_id = aws_vpc.vpc_master.id
+    peer_region = var.region_worker
+}
+
+#Accept VPC peering request in us-west-2 from us-east-1
+resource "aws_vpc_peering_connection_accepter" "accept_peering" {
+  provider                  = aws.region_worker
+  vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
+  auto_accept               = true
+}
+
+#Create route table in us-east-1
+resource "aws_route_table" "internet_route" {
+  provider = aws.region_master
+  vpc_id   = aws_vpc.vpc_master.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_master.id
+  }
+  route {
+    cidr_block                = "10.1.0.0/16"
+    vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Master-Region-RT"
+  }
+}
+
+#Overwrite default route table of VPC(Master) with our route table entries
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  provider       = aws.region_master
+  vpc_id         = aws_vpc.vpc_master.id
+  route_table_id = aws_route_table.internet_route.id
+}
+
+#Create route table in us-west-2
+resource "aws_route_table" "internet_route_worker" {
+  provider = aws.region_worker
+  vpc_id   = aws_vpc.vpc_worker.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_worker.id
+  }
+  route {
+    cidr_block                = "10.0.0.0/16"
+    vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Worker-Region-RT"
+  }
+}
+
+#Overwrite default route table of VPC(Worker) with our route table entries
+resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
+  provider       = aws.region_worker
+  vpc_id         = aws_vpc.vpc_worker.id
+  route_table_id = aws_route_table.internet_route_worker.id
+}
