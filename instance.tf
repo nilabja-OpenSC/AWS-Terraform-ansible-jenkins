0febba63e4f3a6f43245986d6731796a2d35a880
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-05-08 13:23:20 +0530

    Add files via upload

--- instance.tf
+++ instance.tf
@@ -31,6 +31,17 @@ resource "aws_instance" "jenkins-master" {
   }
 
   depends_on = [aws_main_route_table_association.set-master-default-rt-assoc]
+
+#The code below is ONLY the provisioner block which needs to be
+#inserted inside the resource block for Jenkins EC2 master Terraform
+#Jenkins Master Provisioner:
+
+  provisioner "local-exec" {
+    command = <<EOF
+    aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id}
+    ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
+    EOF
+  }
 }
 
 #Create EC2 in us-west-2
@@ -48,6 +59,16 @@ resource "aws_instance" "jenkins-worker" {
     Name = join("_", ["jenkins_worker_tf", count.index + 1])
   }
   depends_on = [aws_main_route_table_association.set-worker-default-rt-assoc, aws_instance.jenkins-master]
+
+#The code below is ONLY the provisioner block which needs to be
+#inserted inside the resource block for Jenkins EC2 worker in Terraform
+
+  provisioner "local-exec" {
+    command = <<EOF
+    aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id}
+    ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-worker-sample.yml
+    EOF
+  }
 }
 
 
