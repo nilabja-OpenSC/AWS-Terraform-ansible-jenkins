c53e80ab3f03db962d8646ee848fd8872d0a999d
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-05-08 13:49:31 +0530

    Add files via upload

--- instance.tf
+++ instance.tf
@@ -38,9 +38,9 @@ resource "aws_instance" "jenkins-master" {
 
   provisioner "local-exec" {
     command = <<EOF
-    aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region_master} --instance-ids ${self.id}
-    ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
-    EOF
+  aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region_master} --instance-ids ${self.id}
+  ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
+  EOF
   }
 }
 
@@ -65,9 +65,9 @@ resource "aws_instance" "jenkins-worker" {
 
   provisioner "local-exec" {
     command = <<EOF
-    aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region_worker} --instance-ids ${self.id}
-    ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-worker-sample.yml
-    EOF
+  aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region_worker} --instance-ids ${self.id}
+  ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-worker-sample.yml
+  EOF
   }
 }
 
